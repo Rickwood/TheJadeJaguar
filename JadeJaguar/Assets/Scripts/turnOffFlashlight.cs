53e2cd1ca17dbb32987c492fce4399fdc431dab3
Rickwood <32183354+Rickwood@users.noreply.github.com>
2018-04-29 19:27:28 -0700

    added script to turn flashlight on and off with the f key

---
+++ JadeJaguar/Assets/Scripts/turnOffFlashlight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnOffFlashlight : MonoBehaviour {
+
+    Light flashlight;
+
+    private void Start()
+    {
+        flashlight = gameObject.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		if(Input.GetKeyDown("f"))
+        {
+                flashlight.enabled = !flashlight.enabled;
+        }
+       
+	}
+}
